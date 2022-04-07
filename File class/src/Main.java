429bc8f154479af2574f288306dcf29dac596997
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-04-07 13:18:51 -0700

    Add files via upload

---
+++ File class/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.File;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		File file = new File("secret_message.txt");
+		
+		if(file.exists()) {
+			
+			System.out.println("that file exists! :D");
+			System.out.println(file.getPath());
+			System.out.println(file.getAbsolutePath());
+			System.out.println(file.isFile());
+			//file.delete();
+			
+		}
+		else {
+			
+			System.out.println("that file doesnt exist :(");
+			
+		}
+
+	}
+
+}
